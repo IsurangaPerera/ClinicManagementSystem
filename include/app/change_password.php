510bf97af74306ad30b1f0e91d49d453f47fdd32
Isuranga Perera <isurangamperera@gmail.com>
2016-04-30 12:18:33 +0530

    alert messages added

--- include/app/change_password.php
+++ include/app/change_password.php
@@ -11,80 +11,93 @@ require('hidden_right.php');
 
 <?php
 if(isset($_POST['submit'])){
+  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+  require("$root/include/config/query.php");
+  require("$root/include/security/security1.php");
+  $oldpwd = test_input($_POST['oldPwd']);
+  $newpwd = test_input($_POST['newPwd']);
+  $newconfirmed = test_input($_POST['conPwd']);
 
- $oldpwd = $_POST['oldPwd'];
- $newpwd = $_POST['newPwd'];
- $newconfirmed = $_POST['conPwd'];
+  if(strcmp($newpwd,$newconfirmed)!==0){
+    ?>  
 
- $root = realpath($_SERVER["DOCUMENT_ROOT"]);
- require("$root/include/config/query.php");
- $make_query = new Query();
- $flag = $make_query->changePassword($oldpwd,$newpwd);
- 
- if($flag < 1){
+    <div class="alert alert-danger center" >
+      <a href = "#" class = "close" data-dismiss = "alert">&times;</a>
+      <strong>Error!</strong> Passwords Doesn't Match.
+    </div>
 
-  ?>
+    <?php
+  } else {
 
-  <script language="javascript">
-  alert("Old password doesn't match");
-  </script>
+    $make_query = new Query();
+    $flag = $make_query->changePassword($oldpwd,$newpwd);
+    if($flag < 1){
 
-  <?php
+      ?>
+
+      <div class="alert alert-danger center" >
+        <a href = "#" class = "close" data-dismiss = "alert">&times;</a>
+        <strong>Error!</strong> Old Password Incorrect.
+      </div>
+
+      <?php
+    } else {
+
+      ?>
+
+      <div id = "myAlert" class = "alert alert-success center">
+       <a href = "#" class = "close" data-dismiss = "alert">&times;</a>
+       <strong>Success!</strong> Password Changed Successfully.
+      </div>
+
+     <?php
+   }
+ }
 }
-}
-
 ?>
 
 <div class="change_password">
   <section class="content">
-   <div class="row">
-    <div class="col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <div id="alert"></div>
+        <div class="box">
+          <div class="box-footer clearfix ">
+          </div>
+          <div class="box-body table-responsive" >
 
-      <div class="box">
-
-        <div class="box-footer clearfix">
+           <form method="post" action="" >
+             <div class="form-group">
+              <label for="exampleInputEmail1">Old Password <font color="#FF0000">*</font></label>
+              <input class="form-control input-sm" name="oldPwd" value="" id="oldPwd" type="password" placeholder="Old Password" style="width: 350px;" required>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">New Password <font color="#FF0000">*</font></label>
+              <input class="form-control input-sm" name="newPwd" id="newPwd" type="password" placeholder="New Password" style="width: 350px;" required>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Confirm Password <font color="#FF0000">*</font></label>
+              <input class="form-control input-sm" name="conPwd" id="conPwd" type="password" placeholder="Confirm Password" style="width: 350px;" required>
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary" name="submit" id="submit" type="submit" ><i class="fa fa-save"></i> Save</button>
+            </div>
+            <input type="hidden" name="truePwd" id="truePwd" value="">
+          </form>
 
         </div>
-
-        <div class="box-body table-responsive">
-
-          <script language="javascript">
-          function copy(){
-           document.getElementById("truePwd").value = document.getElementById("newPwd").value;
-         }
-
-         function validate(evt){
-          if(document.getElementById("newPwd").value .localeCompare(document.getElementById("conPwd")) != 0){
-           evt.preventDefault();
-           alert("Password Not Match");
-           return false;
-         } else {
-           return confirm('Are you sure you want to Change Password');
-         }
-       }
-       </script>
-       <form method="post" action="" onSubmit="return validate();" >
-         <div class="form-group">
-          <label for="exampleInputEmail1">Old Password <font color="#FF0000">*</font></label>
-          <input class="form-control input-sm" name="oldPwd" value="<?php echo $oldpwd; ?>" id="oldPwd" type="password" placeholder="Old Password" style="width: 350px;" required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">New Password <font color="#FF0000">*</font></label>
-          <input class="form-control input-sm" onKeyUp="copy()" name="newPwd" id="newPwd" type="password" placeholder="New Password" style="width: 350px;" required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Confirm Password <font color="#FF0000">*</font></label>
-          <input class="form-control input-sm" name="conPwd" id="conPwd" type="password" placeholder="Confirm Password" style="width: 350px;" required>
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary" name="submit" id="submit" type="submit" ><i class="fa fa-save"></i> Save</button>
-        </div>
-        <input type="hidden" name="truePwd" id="truePwd" value="">
-      </form>
-
+      </div>
     </div>
   </div>
-</div>
-</div>
 </section><!-- /.content -->
 </div>
+<style>
+.center {
+  margin: auto;
+  margin-top: 25px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
+
+
