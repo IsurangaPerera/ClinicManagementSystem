1d85a9756c1d760cb59992092cac9f0a09a2bed2
Isuranga Perera <isurangamperera@gmail.com>
2016-05-01 08:55:42 +0530

    opd doctor module added

---
+++ include/opd_doctor.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+require('config/session_check.php');
+include('config/user_common.php');
+$usr = new User();
+$usr->verifyUser($_SESSION['user_type'], $_SESSION['user_type']);
+require('header.php');
+?>
+
+<ul class="sidebar-menu">
+
+  <li>
+    <a href="?dash_board">
+      <i class="fa fa-dashboard"></i> <span><strong>Dashboard</strong></span>
+    </a>
+  </li>     
+
+ <li>
+    <a href="?dash_board">
+      <i class="fa fa-dashboard"></i> <span><strong>Patient Management</strong></span>
+    </a>
+  </li> 
+
+
+  <!--START OF USER PROFILE-->
+  <li id="lst" class="treeview ">
+    <a href="#">
+      <i class="fa fa-user"></i> <span><strong>User Profile</strong></span>
+      <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul id="slst" class="treeview-menu">
+      <li ><a href="?my_profile"><i class="fa fa-angle-double-right"></i>My Profile</a></li>
+      <li ><a href="?edit_profile"><i class="fa fa-angle-double-right"></i>Edit Profile</a></li>
+      <li ><a href="?change_password"><i class="fa fa-angle-double-right"></i>Change Password</a></li>
+      <li ><a href="config/logout.php"><i class="fa fa-angle-double-right"></i>Logout</a></li>
+    </ul>
+  </li>
+  <!--END OF USER PROFILE-->
+</ul>
+
+</section>
+<!-- /.sidebar -->
+</aside>
+
+<!-- Right side column. Contains the navbar and content of the page -->
+<aside class="right-side" >
+
+  <!-- Content Header (Page header) -->
+  <!--<section class="content-header" >
+    <h1>DashBoard</h1> 
+  </section>-->
+
+  <?php include('app/dash_board.php'); ?>
+
+  <?php
+  if(isset($_GET['manage_appointments'])) {
+    include('app/manage_appointments.php');
+  } 
+
+  if(isset($_GET['my_profile'])) {
+    include('app/my_profile.php');
+  }
+
+  if(isset($_GET['edit_profile'])) {
+    include('app/edit_profile.php');
+  }
+
+  if(isset($_GET['search_patient'])) {
+    include('app/search_patient.php');
+  }
+
+  if(isset($_GET['patient_registration'])) {
+    include('app/patient_registration.php');
+  }
+
+  if(isset($_GET['dash_board'])) {
+    require('app/hidden_right.php');
+    ?>
+
+    <style>
+    .dash_board, .opt{
+      display: block;
+    }
+    </style>
+    
+    <?php
+
+  }
+
+  if(isset($_GET['change_password'])) {
+    include('app/change_password.php');
+  }
+
+  ?>
+  <!-- Main content -->
+</aside><!-- /.right-side -->
+</div><!-- ./wrapper -->
+
+<script src="https://code.jquery.com/jquery-1.9.1.min.js" integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ="   crossorigin="anonymous"></script>
+<script src="../css/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>     
+<script src="../js/app.js" type="text/javascript"></script>
+
+<!-- BDAY -->
+<script src="../../dp/js/dp.js"></script>
+
+<script type="text/javascript">
+            // When the document is ready
+            $(document).ready(function () {
+
+              $('#cFrom').datepicker({
+                    //format: "dd/mm/yyyy"
+                    format: "yyyy-mm-dd"
+                  });  
+
+              $('#cTo').datepicker({
+                    //format: "dd/mm/yyyy"
+                    format: "yyyy-mm-dd"
+                  });  
+            });
+            </script>
+            <!-- END BDAY -->
+
+
+</body>
+</html>
